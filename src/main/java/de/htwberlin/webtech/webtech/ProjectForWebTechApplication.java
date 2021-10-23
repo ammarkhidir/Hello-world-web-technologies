a8f2d0f7719f7d12553277cc3c78a8a368d32f36
ammarkhidir <s0580617@htw-berlin.de>
2021-10-23 13:08:15 +0200

    first commit

---
+++ src/main/java/de/htwberlin/webtech/webtech/ProjectForWebTechApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForWebTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForWebTechApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/htwberlin/webtech/webtech/web/HelloWorldController.java
@@ -0,0 +1,18 @@
+package de.htwberlin.webtech.webtech.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+
+}
