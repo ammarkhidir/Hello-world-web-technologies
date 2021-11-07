1f1e2c7d536a8fe86432b265c1db79d411fe926e
ammarkhidir <s0580617@htw-berlin.de>
2021-11-07 21:17:16 +0100

    last one

--- src/main/java/de/htwberlin/webtech/webtech/web/HelloWorldController.java
+++ src/main/java/de/htwberlin/webtech/webtech/web/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("")
+    @GetMapping("/helloworld")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "helloworld";
