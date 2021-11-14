855bb80cd46a6c295a307744ba68bd40cb9d3767
ammarkhidir <s0580617@htw-berlin.de>
2021-11-14 20:18:13 +0100

    Finish Milestone 2

--- src/main/java/de/htwberlin/webtech/webtech/web/api/PersonManipulationRequest.java
+++ src/main/java/de/htwberlin/webtech/webtech/web/api/PersonManipulationRequest.java
@@ -1,24 +1,15 @@
 package de.htwberlin.webtech.webtech.web.api;
 
-public class  Person {
-    private long id;
+public class PersonManipulationRequest {
+
     private String firstName;
     private String lastName;
     private boolean vaccinated;
 
-    public Person(long id, String firstName, String lastName, boolean vaccinated) {
-        this.id = id;
+    public PersonManipulationRequest(String firstName, String lastName, boolean vaccinated) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.vaccinated = vaccinated;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
